8c152f30121c813e8920ae043f173f27b6422d29
Brett Mack <mack.brett@gmail.com>
2013-04-26 09:47:31 +0100

    First version

--- config.rb
+++ config.rb
@@ -4,10 +4,10 @@ require 'sass-globbing'
 project_type = :stand_alone
 
 # Publishing paths
-http_path = "/"
-http_images_path = "/images"
-http_fonts_path = "/fonts"
-css_dir = "public/stylesheets"
+http_path = "/foxieblog/"
+http_images_path = "/foxieblog/images"
+http_fonts_path = "/foxieblog/fonts"
+css_dir = "public/foxieblog/stylesheets"
 
 # Local development paths
 sass_dir = "sass"
---
+++ source/_posts/2013-04-26-my-first-post-and-an-introduction-to-my-task.markdown
@@ -0,0 +1,14 @@
+---
+layout: post
+title: "My First Post and an Introduction To My Task"
+date: 2013-04-26 06:57
+comments: true
+categories: 
+  - Introduction
+  - Continuous Delivery
+---
+w00, my *first* post **yay yay**! [Foxienet][1]
+
+Here, have an image: {% img /images/foxie1.jpg Totally cute foxie %}
+
+[1]: http://www.foxienet.com
